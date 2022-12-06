d7676c4fdc4e3b3433f2308ba786fc69c848b6bd
Henning Meyer <tutmann@gmail.com>
2022-12-06 06:59:00 +0100

    d06 added

---
+++ d06/src/main.rs
@@ -0,0 +1,33 @@
+use std::io;
+use std::collections::{HashSet, VecDeque};
+use std::ops::RangeInclusive;
+
+
+fn parse_line(l: &str) -> usize {
+    let mut b = VecDeque::new();
+    const L:usize = 14;
+
+    for (i, c) in l.chars().enumerate() {
+        if i >= L {
+            b.pop_front();
+        }
+        b.push_back(c);
+        let mut s = HashSet::new();
+        for x in b.iter() {
+            s.insert(x);
+        }
+        if s.len() == L {
+            return i + 1;
+        }
+    }
+    panic!("damn!");
+}
+
+
+fn main() -> io::Result<()> {
+    let stdin = io::stdin();
+    for l in stdin.lines() {
+        println!("{}", parse_line(&l.unwrap()));
+    }
+    Ok(())
+}
