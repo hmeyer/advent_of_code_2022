dcad785c944ad579a88c6697e84f54b2a788ac87
Henning Meyer <tutmann@gmail.com>
2022-12-26 08:47:06 +0100

    implemented time as neg_time to be able to derive Ord

--- d24/src/main.rs
+++ d24/src/main.rs
@@ -20,37 +20,32 @@ fn parse_blizzards(lines: &[String]) -> (Vec2, HashSet<(char, i32, i32)>) {
     (Vec2 {x: lines[0].len() as i32, y: lines.len() as i32}, blizzards)
 }
 
-#[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Hash, Ord, PartialOrd)]
 struct Vec2 {
     x: i32,
     y: i32,
 }
 
-#[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Hash, Ord, PartialOrd)]
 struct State {
-    time: i32,
+    neg_time: i32,
     p: Vec2,
 }
 
-impl Ord for State {
-    fn cmp(&self, other: &Self) -> Ordering {
-        (other.time).cmp(&self.time)
-            .then_with(|| self.p.x.cmp(&other.p.x))
-            .then_with(|| self.p.y.cmp(&other.p.y))
+impl State {
+    fn new(time: i32, p: Vec2) -> Self {
+        State { neg_time: -time, p }
     }
-}
-
-impl PartialOrd for State {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
+    fn time(&self) -> i32 {
+        -self.neg_time
     }
 }
 
 fn has_blizzard(s: &State, size: Vec2, blizzards: &HashSet<(char, i32, i32)>) -> bool {
-    blizzards.get(&('>', (s.p.x - s.time).rem_euclid(size.x), s.p.y)).is_some() ||
-    blizzards.get(&('<', (s.p.x + s.time).rem_euclid(size.x), s.p.y)).is_some() ||
-    blizzards.get(&('v', s.p.x, (s.p.y - s.time).rem_euclid(size.y))).is_some() ||
-    blizzards.get(&('^', s.p.x, (s.p.y + s.time).rem_euclid(size.y))).is_some()
+    blizzards.get(&('>', (s.p.x - s.time()).rem_euclid(size.x), s.p.y)).is_some() ||
+    blizzards.get(&('<', (s.p.x + s.time()).rem_euclid(size.x), s.p.y)).is_some() ||
+    blizzards.get(&('v', s.p.x, (s.p.y - s.time()).rem_euclid(size.y))).is_some() ||
+    blizzards.get(&('^', s.p.x, (s.p.y + s.time()).rem_euclid(size.y))).is_some()
 }
 
 fn reconstruct(mut p: State, came_from: &HashMap<State, State>) -> Vec<Vec2> {
@@ -73,7 +68,7 @@ fn dijkstra(start: State, goal: Vec2, size: Vec2, blizzards: &HashSet<(char, i32
     };
     let mut heap = BinaryHeap::new();
     let mut dist = HashMap::new();
-    dist.insert((start.p, start.time % lcm), start.time);
+    dist.insert((start.p, start.time() % lcm), start.time());
     heap.push(start);
 
     let mut came_from = HashMap::new();
@@ -83,8 +78,8 @@ fn dijkstra(start: State, goal: Vec2, size: Vec2, blizzards: &HashSet<(char, i32
             return reconstruct(current, &came_from);
         }
 
-        if let Some(d) = dist.get(&(current.p, current.time % lcm)) {
-            if current.time > *d {
+        if let Some(d) = dist.get(&(current.p, current.time() % lcm)) {
+            if current.time() > *d {
                 continue;
             }
         }
@@ -101,15 +96,15 @@ fn dijkstra(start: State, goal: Vec2, size: Vec2, blizzards: &HashSet<(char, i32
                         continue;
                     }
                 }
-                let next = State { time: current.time + 1, p: next_pos};
+                let next = State::new(current.time() + 1, next_pos);
                 if has_blizzard(&next, size, blizzards) {
                     continue;
                 }
-                let d = dist.entry((next.p, next.time % lcm)).or_insert(i32::MAX);
-                if next.time < *d {
+                let d = dist.entry((next.p, next.time() % lcm)).or_insert(i32::MAX);
+                if next.time() < *d {
                     came_from.insert(next, current);
                     heap.push(next);
-                    *d = next.time;
+                    *d = next.time();
                 }
             }
         }
@@ -120,16 +115,16 @@ fn dijkstra(start: State, goal: Vec2, size: Vec2, blizzards: &HashSet<(char, i32
 
 fn get_blizzard(s: &State, size: Vec2, blizzards: &HashSet<(char, i32, i32)>) -> char {
     let mut b = Vec::new();
-    if blizzards.get(&('>', (s.p.x - s.time).rem_euclid(size.x), s.p.y)).is_some() {
+    if blizzards.get(&('>', (s.p.x - s.time()).rem_euclid(size.x), s.p.y)).is_some() {
         b.push('>');
     }
-    if blizzards.get(&('<', (s.p.x + s.time).rem_euclid(size.x), s.p.y)).is_some() {
+    if blizzards.get(&('<', (s.p.x + s.time()).rem_euclid(size.x), s.p.y)).is_some() {
         b.push('<');
     }
-    if blizzards.get(&('v', s.p.x, (s.p.y - s.time).rem_euclid(size.y))).is_some() {
+    if blizzards.get(&('v', s.p.x, (s.p.y - s.time()).rem_euclid(size.y))).is_some() {
         b.push('v');
     }
-    if blizzards.get(&('^', s.p.x, (s.p.y + s.time).rem_euclid(size.y))).is_some() {
+    if blizzards.get(&('^', s.p.x, (s.p.y + s.time()).rem_euclid(size.y))).is_some() {
         b.push('^');
     }
     match b.len() {
@@ -148,7 +143,7 @@ fn print_map(size: Vec2, e: Vec2, t: i32, blizzards: &HashSet<(char, i32, i32)>)
     for y in 0..size.y {
         print!("#");
         for x in 0..size.x {
-            let b = get_blizzard(&State{time: t, p: Vec2 { x, y }}, size, blizzards);
+            let b = get_blizzard(&State::new(t, Vec2 { x, y }), size, blizzards);
             if e == (Vec2 { x, y }) {
                 if b != '.' {
                     panic!("Collision at {:?} time: {}", (x, y), t);
@@ -182,19 +177,19 @@ fn main() -> io::Result<()> {
     
     let start = Vec2 { x: 0, y: -1};
     let goal = Vec2 { x: size.x - 1, y: size.y - 1};
-    let p1 = dijkstra(State{time: 0, p:start}, goal, size, &blizzards);
+    let p1 = dijkstra(State::new(0, start), goal, size, &blizzards);
     // print_path(size, &p, &blizzards);
     println!("start->goal takes {} Minutes", p1.len());
 
 
     let start = Vec2 { x: size.x - 1, y: size.y};
     let goal = Vec2 { x: 0, y: 0};
-    let p2 = dijkstra(State{time: p1.len() as i32, p:start}, goal, size, &blizzards);
+    let p2 = dijkstra(State::new(p1.len() as i32, start), goal, size, &blizzards);
     println!("start<-goal takes {} Minutes", p2.len());
 
     let start = Vec2 { x: 0, y: -1};
     let goal = Vec2 { x: size.x - 1, y: size.y - 1};
-    let p3 = dijkstra(State{time: (p1.len() + p2.len()) as i32, p:start}, goal, size, &blizzards);
+    let p3 = dijkstra(State::new((p1.len() + p2.len()) as i32, start), goal, size, &blizzards);
     println!("start->goal takes {} Minutes", p3.len());
 
     println!("total time: {} + {} + {} = {}", p1.len(), p2.len(), p3.len(), p1.len() + p2.len() + p3.len());
